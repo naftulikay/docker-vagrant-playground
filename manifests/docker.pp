61409964c7b53e4089f132f761be307f448db10e
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2014-03-31 22:38:35 -0700

    Added the Puppet apt module and dependencies, created Puppet provisioning for installing Docker, its repository, and the right kernel.

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,4 +1,22 @@
 # Puppet for Docker Vagrant Box
 node default {
+    # apt
+    class { 'apt': }
     
+    apt::source { 'docker':
+        location => "http://get.docker.io/ubuntu",
+        key => "36A1D7869245C8950F966E92D8576A8BA88D21E9",
+        release => "docker",
+        repos => "main",
+        include_src => false
+    }
+
+    package { 'raring-kernel':
+        name => 'linux-image-generic-lts-raring',
+        ensure => present
+    }
+
+    package { 'lxc-docker':
+        require => [apt::source["docker"], Package["raring-kernel"]]
+    }
 }
